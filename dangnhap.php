8c1c1221d53a4aa15f766c60435668fc23d7e0de
ThanhTung-58TH2 <45089231+ThanhTung-58TH2@users.noreply.github.com>
2018-12-18 13:57:50 +0700

    Create dangnhap.php

---
+++ dangnhap.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>Trang đăng nhập</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<form method="POST" action="dangnhap.php">
+	<fieldset styde="width:100px;">
+	    <legend>Đăng nhập</legend>
+	    	<table>
+	    		<tr>
+	    			<td>Username</td>
+	    			<td><input type="text" name="username" ></td>
+	    		</tr>
+	    		<tr>
+	    			<td>Password</td>
+	    			<td><input type="password" name="password" ></td>
+	    		</tr>
+	    		<tr>
+	    			<td colspan="2" align="center"> <input type="submit" value="Đăng nhập"></td>
+	    		</tr>
+	    	</table>
+  </fieldset>
+  </form>
+</body>
+</html>
